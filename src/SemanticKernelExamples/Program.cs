d7bf9f37dc7bda6da48a349bde3c33bc6ecd0cae
Tim Miller <innerlogic4321@gmail.com>
2023-09-06 13:43:05 +0900

    Update

--- src/SemanticKernelExamples/Program.cs
+++ src/SemanticKernelExamples/Program.cs
@@ -5,13 +5,39 @@ internal class Program
     private static async Task Main(string[] args)
     {
         Console.WriteLine("Please input a number to choose an example to run:");
+        Console.WriteLine("0: Internet");
+        Console.WriteLine("1: Local");
+
+        while(true)
+        {
+            Console.Write("\nYour choice: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            if (choice == 0)
+            {
+                await Runner(true);
+            }
+            else if (choice == 1)
+            {
+                await Runner(false);
+            }
+            else
+            {
+                Console.WriteLine("Cannot parse your choice. Please select again.");
+                continue;
+            }
+            break;
+        }
+    }
+
+    private static async Task Runner(bool internet)
+    {
+        Console.WriteLine("Please input a number to choose an example to run:");
         Console.WriteLine("0: Example04_CombineLLMPromptsAndNativeCode");
         Console.WriteLine("1: Example14_SemanticMemory");
         Console.WriteLine("2: Example17_ChatGPT");
 
-        var internet = false;
-
-        while(true)
+        while (true)
         {
             Console.Write("\nYour choice: ");
             int choice = int.Parse(Console.ReadLine());
